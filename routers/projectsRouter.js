5baf65dc26fdca6f600eae0099ce065479c3443a
Dominick Bruno <dbruno@unseen.is>
2019-11-09 10:15:52 -0500

    converted req.params.id to req.params

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -18,13 +18,13 @@ const projectDb = require('../data/helpers/projectModel.js');
             res
             .status(500)
             .json({
-                message: 'The Projects information could not be retrieved.',
-            });
-        });
-    })
+                message: 'The Projects information could not be retrieved.',});
+        })
+    });
     //GET project by id
     router.get('/:id', (req, res) => {
-        projectDb.get(req.params.id)
+
+        projectDb.get(req.params)
         .then(project => {
             res
             .status(200)
@@ -36,12 +36,11 @@ const projectDb = require('../data/helpers/projectModel.js');
             res
             .status(500)
             .json({
-                message: 'The Action information could not be retrieved.',
+                message: 'The Project information could not be retrieved.',});
             });
-            });
-    })
+    });
 
-    // POST call to api
+    // POST Call
 
     router.post('/', (req, res) => {
         projectDb.insert(req.body)
@@ -56,9 +55,77 @@ const projectDb = require('../data/helpers/projectModel.js');
           res
           .status(500)
           .json({
-            message: 'Error adding the project.',
-          });
+            message: 'Error adding the project.',});
         });
       });
+
+// PUT Handler
+router.put('/:id', (req, res) => {
+    projectDb.update(req.params.id, req.body)
+    .then(project => {
+      if (project) {
+        res
+        .status(200)
+        .json(project);
+      } else {
+        res
+        .status(404)
+        .json({ message: 'The project could not be found.', });
+      }
+    })
+    .catch(error => {
+      // log error to server
+      console.log(error);
+      res
+      .status(500)
+      .json({
+        message: 'Error updating the project.',});
+    });
+  });
+
+      // DELETE Request Handlers
+// Using async/await as the server has to take action
+
+        // DELETE a Post by Id
+        router.delete('/:id', (req, res) => {
+            projectDb.remove(req.params)
+            .then(count => {
+              if (count > 0) {
+                res
+                .status(200)
+                .json({ message: 'The project has been nuked', });
+              } else {
+                res
+                .status(404)
+                .json({ message: 'The project could not be found', });
+              }
+            })
+            .catch(error => {
+              // log error to server
+              console.log(error);
+              res
+              .status(500)
+              .json({
+                message: 'Error removing the project',});
+            });
+          });
+
+          // Get Project Actions
+router.get("/:id/actions", validateProjectId, async (req, res) => {
+  try {
+    const actions = await db.getProjectActions(req.params.id);
+
+    actions.length > 0
+      ? res.status(200).json(actions)
+      : res
+          .status(200)
+          .json({ message: "This projects doesn't have any actions" });
+  } catch {
+    res.status(500).json({
+      error:
+        "There was an error while attempting to fetch the actions for the project"
+    });
+  }
+});
 // export router
 module.exports = router;
